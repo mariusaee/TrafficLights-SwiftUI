09b3148a66a17e2a6e75debc197980387b1e745e
mariusaee <mariusae@me.com>
2021-06-01 18:04:03 +0300

    Initial Commit

---
+++ TrafficLights SwiftUI/TrafficLights_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLights_SwiftUIApp.swift
+//  TrafficLights SwiftUI
+//
+//  Created by Marius Malyshev on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLights_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
